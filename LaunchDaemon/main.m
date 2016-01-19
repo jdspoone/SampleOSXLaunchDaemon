631aa3e6a9d0e7caba759929d7d2c8fb1d391ecf
Jeff Spooner <jdspoone@icloud.com>
2016-01-19 10:58:08 -0700

    Initial Commit

---
+++ LaunchDaemon/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LaunchDaemon
+//
+//  Created by Jeffrey Spooner on 2016-01-19.
+//  Copyright © 2016 Example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
